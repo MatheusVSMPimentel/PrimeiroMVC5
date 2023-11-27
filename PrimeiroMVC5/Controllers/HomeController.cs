881d943d4c60490e8d40328b59c9f55704ecab16
MatheusVSMPimentel <matheusvsm.pimentel@hotmail.com>
2023-11-27 19:41:13 -0300

    Add project files.

---
+++ PrimeiroMVC5/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeiroMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult OlaMundo()
+        {
+            return Content("Ola Mundo");
+        }
+
+        public FileContentResult ImagemJpg()
+        {
+            var foto = System.IO.File.ReadAllBytes(Server.MapPath("/content/images/capa.jpg"));
+            return File(foto, "image/jpg", "capa.jpg");
+        }
+
+        public HttpUnauthorizedResult LoginFailed()
+        {
+            return new HttpUnauthorizedResult();
+        }
+
+        public JsonResult JsonResult()
+        {
+            return Json("teste:'teste'", JsonRequestBehavior.AllowGet);
+        }
+
+        public RedirectResult Redirect()
+        {
+            return new RedirectResult("https://matheuspimentelcv.netlify.com");
+        }
+
+        public RedirectToRouteResult RedirectRoute()
+        {
+            return RedirectToRoute(new
+            {
+                controller = "Home",
+                action = "Index"
+            });
+        }
+
+        public RedirectToRouteResult RedirectRouteAction()
+        {
+            return RedirectToAction("Index","Test");
+        }
+    }
+}
---
+++ PrimeiroMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrimeiroMVC5.MvcApplication" Language="C#" %>
