77c0773a9a53d0ad603991c8eecc3ba4e58ccc7d
MatheusVSMPimentel <matheusvsm.pimentel@hotmail.com>
2023-12-01 15:03:28 -0300

    Create CRUD to Student Model. **END OF PLAYGROUND MVC5** Stay on for the next episodes

--- PrimeiroMVC5/Controllers/StudentController.cs
+++ PrimeiroMVC5/Controllers/StudentController.cs
@@ -1,7 +1,10 @@
-﻿using PrimeiroMVC5.Models;
+﻿using PrimeiroMVC5.Data;
+using PrimeiroMVC5.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +12,14 @@ namespace PrimeiroMVC5.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly AppDbContext _appDbContext;
+
+        public StudentController()
+        {
+            _appDbContext = new AppDbContext();
+        }
+
+
         // GET: Aluno
         [Route("new-student")]
         public ActionResult New(Student aluno)
@@ -27,7 +38,7 @@ namespace PrimeiroMVC5.Controllers
         }
         public ActionResult Index(Student aluno)
         {
-            if(!ModelState.IsValid) return View(aluno);
+            if (!ModelState.IsValid) return View(aluno);
             return View(aluno);
         }
 
@@ -36,12 +47,55 @@ namespace PrimeiroMVC5.Controllers
         {
             return View();
         }
+
         [HttpPost]
         [Route("add-new-student")]
-        public ActionResult AddStudent(Student student)
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> AddStudent(Student student)
         {
             if (!ModelState.IsValid) return View(student);
+            if (!ModelState.IsValid) return View(student);
+            student.SignupDate = DateTime.Now;
+            _appDbContext.Students.Add(student);
+            await _appDbContext.SaveChangesAsync();
             return View(student);
+        }
+
+        [HttpPost]
+        [Route("get-students")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> GetStudents()
+        {
+            var students = _appDbContext.Students.ToList();
+            return View(students);
+        }
+
+        [HttpGet]
+        [Route("edit-student")]
+        public async Task<ActionResult> EditStudent()
+        {
+            var student = _appDbContext.Students.FirstOrDefault(a => a.Name == "asd");
+            student.Name = "João";
+            var entry = _appDbContext.Entry(student);
+            _appDbContext.Set<Student>().Attach(student);
+            entry.State = EntityState.Modified;
+            _appDbContext.SaveChanges();
+
+            var newStudent = _appDbContext.Students.Find(student.Id);
+            return View("AddStudent", newStudent);
+        }
+
+
+
+        [HttpGet]
+        [Route(template: "delete-student")]
+        public ActionResult ExcluirAluno()
+        {
+            var student = _appDbContext.Students.FirstOrDefault(a => a.Name == "João");
+            _appDbContext.Students.Remove(student);
+            _appDbContext.SaveChanges();
+            var students = _appDbContext.Students.ToList();
+            return View("AddStudent", model: students.FirstOrDefault());
         }
     }
 }
